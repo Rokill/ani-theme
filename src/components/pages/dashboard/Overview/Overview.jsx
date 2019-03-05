75653ae8ad32a503c18bc430a2333413fcddf7c9
Stanislav <szabielin@gmail.com>
2019-03-05 11:09:27 +0200

    Update Overview.jsx

--- src/components/pages/dashboard/Overview/Overview.jsx
+++ src/components/pages/dashboard/Overview/Overview.jsx
@@ -3,16 +3,32 @@ import { Link } from "react-router";
 import {Jumbotron} from 'react-bootstrap';
 
 var Blank = React.createClass({
+  componentDidMount() {
+    fetch("https://reactnodb.herokuapp.com/api.json")
+      .then(res => res.json())
+      .then(res => this.setState({ data: res }))
+      .catch(err => console.log(err));
+  };
   render: function() {
     return (
-      <div className="overview-page" key="overview"> 
-        <Link to="/dashboard/reports" className="pull-right btn btn-primary btn-outline btn-rounded">Reports</Link> 
-        <h2>Overview <small>Why is Ani Theme so awesome?</small></h2> 
-        <Jumbotron> 
-          <h1>Welcome!</h1> I don't want to bloat this Ani Theme Free Edition AngularJS Admin theme with a lot of plugins. To install plugins we have bower. Let's keep it real clean!
-          <br /><br /> 
-          <p> <a className="btn btn-primary btn-lg btn-outline btn-rounded">Learn more</a> </p> 
-        </Jumbotron> 
+       <div className={styles.Main}>
+        <h1>Database API</h1>
+        <div className={styles.Container}>
+          <h2>Data</h2>
+          {this.state.data.map((item, i) => {
+            return (
+              <div key={i} className={styles.Data}>
+                <ul>
+                  <li>DID: {item.did}</li>
+				  <li>Name: {item.name}</li>
+                  {/* <li>Name: {item.name</li> */}
+                  {/* <li>Age: {item.age}</li> */}
+                  {/* <li>Password: {item.pass}</li> */}
+                </ul>
+              </div>
+            );
+          })}
+        </div>
       </div>
       
       
